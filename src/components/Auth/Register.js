e18454b43eefceb3c8a3d3275febf01d85bb5f78
JoeVincentt <eugene.bb@hotmail.com>
2019-01-08 17:35:43 -0500

    working on form submit adding logic for validation and displaying errors

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -9,28 +9,77 @@ class Register extends Component {
     username: "",
     email: "",
     password: "",
-    passwordConfirmation: ""
+    passwordConfirmation: "",
+    errors: []
   };
+
+  isFormValid = () => {
+    let errors = [];
+    let error;
+
+    if (this.isFormEmpty(this.state)) {
+      error = { message: "Fill in all fields" };
+      this.setState({ errors: errors.concat(error) });
+      return false;
+    } else if (!this.isPasswordValid(this.state)) {
+      error = { message: "Pasword is invalid" };
+      this.setState({ errors: errors.concat(error) });
+      return false;
+    } else {
+      //form valid
+      return true;
+    }
+  };
+
+  isFormEmpty = ({ email, username, password, passwordConfirmation }) => {
+    return (
+      !username.length ||
+      !email.length ||
+      !password.length ||
+      !passwordConfirmation.length
+    );
+  };
+
+  isPasswordValid = ({ password, passwordConfirmation }) => {
+    if (password.length < 6 || passwordConfirmation.length < 6) {
+      return false;
+    } else if (password !== passwordConfirmation) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  displayErrors = errors =>
+    errors.map((error, i) => <p key={i}>{error.message}</p>);
 
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
 
   handleSubmit = event => {
-    event.preventDefault();
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(this.state.email, this.state.password)
-      .then(createdUser => {
-        console.log(createdUser);
-      })
-      .catch(err => {
-        console.log(err);
-      });
+    if (this.isFormValid()) {
+      event.preventDefault();
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then(createdUser => {
+          console.log(createdUser);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
   };
 
   render() {
-    const { username, password, passwordConfirmation, email } = this.state;
+    const {
+      username,
+      password,
+      passwordConfirmation,
+      email,
+      errors
+    } = this.state;
 
     return (
       <Grid textAlign="center" verticalAlign="middle" className="app">
@@ -88,10 +137,17 @@ class Register extends Component {
                 Submit
               </Button>
             </Segment>
-            <Message>
-              Already a user? <Link to="/login">Login</Link>{" "}
-            </Message>
           </Form>
+          {errors.length > 0 && (
+            <Message error>
+              <h3>Error</h3>
+              {this.displayErrors(errors)}
+            </Message>
+          )}
+
+          <Message>
+            Already a user? <Link to="/login">Login</Link>{" "}
+          </Message>
         </Grid.Column>
       </Grid>
     );
