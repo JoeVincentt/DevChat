5b8c7ae6558efacbbea080c6c68527834f993917
JoeVincentt <eugene.bb@hotmail.com>
2019-01-09 17:24:16 -0500

    set first channel and show active channel upon loading app

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -13,7 +13,9 @@ class Channels extends Component {
     channelDetails: "",
     channelsRef: firebase.database().ref("channels"),
     modal: false,
-    loading: false
+    loading: false,
+    firstLoad: true,
+    activeChannel: ""
   };
 
   componentDidMount() {
@@ -24,8 +26,17 @@ class Channels extends Component {
     let loadedChannels = [];
     this.state.channelsRef.on("child_added", snap => {
       loadedChannels.push(snap.val());
-      this.setState({ channels: loadedChannels });
+      this.setState({ channels: loadedChannels }, () => this.setFirstChannel());
     });
+  };
+
+  setFirstChannel = () => {
+    const firstChannel = this.state.channels[0];
+    if (this.state.firstLoad && this.state.channels.length > 0) {
+      this.props.setCurrentChannel(firstChannel);
+      this.setActiveChannel(firstChannel);
+    }
+    this.setState({ firstLoad: false });
   };
 
   handleSubmit = event => {
@@ -76,7 +87,12 @@ class Channels extends Component {
   };
 
   changeChannel = channel => {
+    this.setActiveChannel(channel);
     this.props.setCurrentChannel(channel);
+  };
+
+  setActiveChannel = channel => {
+    this.setState({ activeChannel: channel.id });
   };
 
   openModal = () => this.setState({ modal: true });
@@ -90,6 +106,7 @@ class Channels extends Component {
         onClick={() => this.changeChannel(channel)}
         name={channel.name}
         style={{ opacity: 0.7 }}
+        active={channel.id === this.state.activeChannel}
       >
         # {channel.name}
       </MenuItem>
