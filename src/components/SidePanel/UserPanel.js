5a37608ef3299613c7a3a0a9a2f2cde3ba50c15e
JoeVincentt <eugene.bb@hotmail.com>
2019-01-08 21:30:54 -0500

    display avatar

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -6,7 +6,8 @@ import {
   HeaderContent,
   GridRow,
   Icon,
-  Dropdown
+  Dropdown,
+  Image
 } from "semantic-ui-react";
 import firebase from "../../firebase";
 
@@ -43,6 +44,8 @@ class UserPanel extends Component {
   };
 
   render() {
+    const { user } = this.state;
+
     return (
       <Grid style={{ background: "#4c3c4c" }}>
         <GridColumn>
@@ -52,14 +55,19 @@ class UserPanel extends Component {
               <Icon name="code" />
               <HeaderContent>DevChat</HeaderContent>
             </Header>
+            {/* User Dropdows */}
+            <Header style={{ padding: "0.25em" }} as="h4" inverted>
+              <Dropdown
+                trigger={
+                  <span>
+                    <Image src={user.photoURL} spaced="right" avatar />
+                    {user.displayName}
+                  </span>
+                }
+                options={this.dropdownOptions()}
+              />
+            </Header>
           </GridRow>
-          {/* User Dropdows */}
-          <Header style={{ padding: "0.25em" }} as="h4" inverted>
-            <Dropdown
-              trigger={<span>{this.state.user.displayName}</span>}
-              options={this.dropdownOptions()}
-            />
-          </Header>
         </GridColumn>
       </Grid>
     );
