abdac0961981d920576805576bcf706fb4454f7f
JoeVincentt <eugene.bb@hotmail.com>
2019-01-09 20:34:33 -0500

    creating modal to upload files

---
+++ src/components/Messages/FileModal.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+//prettier-ignore
+import { Modal, ModalHeader, ModalContent, Input, ModalActions, Button, Icon } from 'semantic-ui-react';
+
+class FileModal extends Component {
+  render() {
+    const { modal, closeModal } = this.props;
+
+    return (
+      <Modal basic open={modal} onClose={closeModal}>
+        <ModalHeader>Select an Image File</ModalHeader>
+        <ModalContent>
+          <Input fluid label="File types: jpg, png" name="file" type="file" />
+        </ModalContent>
+
+        <ModalActions>
+          <Button color="green" inverted>
+            <Icon name="checkmark" /> Send
+          </Button>
+
+          <Button color="red" inverted onClick={closeModal}>
+            <Icon name="remove" /> Cancel
+          </Button>
+        </ModalActions>
+      </Modal>
+    );
+  }
+}
+export default FileModal;
--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Segment, Input, Button, ButtonGroup } from "semantic-ui-react";
 import firebase from "../../firebase";
+import FileModal from "./FileModal";
 
 class MessageForm extends Component {
   state = {
@@ -8,8 +9,13 @@ class MessageForm extends Component {
     loading: false,
     errors: [],
     channel: this.props.currentChannel,
-    user: this.props.currentUser
+    user: this.props.currentUser,
+    modal: false
   };
+
+  openModal = () => this.setState({ modal: true });
+
+  closeModal = () => this.setState({ modal: false });
 
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
@@ -56,7 +62,7 @@ class MessageForm extends Component {
   };
 
   render() {
-    const { errors, message, loading } = this.state;
+    const { errors, message, loading, modal } = this.state;
     return (
       <Segment className="message__form">
         <Input
@@ -84,11 +90,13 @@ class MessageForm extends Component {
             icon="edit"
           />
           <Button
+            onClick={this.openModal}
             color="teal"
             content="Upload Media"
             labelPosition="right"
             icon="cloud upload"
           />
+          <FileModal modal={modal} closeModal={this.closeModal} />
         </ButtonGroup>
       </Segment>
     );
