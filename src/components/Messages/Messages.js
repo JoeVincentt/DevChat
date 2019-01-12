fc6823a20dbb46198523ce1283187c4c67db10a0
JoeVincentt <eugene.bb@hotmail.com>
2019-01-12 00:30:52 -0500

    add typing listener, to see when other users are typing

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -14,6 +14,8 @@ class Messages extends Component {
     privateMessagesRef: firebase.database().ref("privateMessages"),
     messagesRef: firebase.database().ref("messages"),
     usersRef: firebase.database().ref("users"),
+    typingRef: firebase.database().ref("typing"),
+    connectedRef: firebase.database().ref(".info/connected"),
     channel: this.props.currentChannel,
     user: this.props.currentUser,
     messages: [],
@@ -23,7 +25,8 @@ class Messages extends Component {
     searchTerm: "",
     searchLoading: false,
     searchResults: [],
-    isChannelStarred: false
+    isChannelStarred: false,
+    typingUsers: []
   };
 
   componentDidMount() {
@@ -36,6 +39,42 @@ class Messages extends Component {
 
   addListeners = channelId => {
     this.addMessageListener(channelId);
+    this.addTypingListeners(channelId);
+  };
+
+  addTypingListeners = channelId => {
+    let typingUsers = [];
+    this.state.typingRef.child(channelId).on("child_added", snap => {
+      if (snap.key !== this.state.user.uid) {
+        typingUsers = typingUsers.concat({
+          id: snap.key,
+          name: snap.val()
+        });
+        this.setState({ typingUsers });
+      }
+    });
+
+    this.state.typingRef.child(channelId).on("child_removed", snap => {
+      const index = typingUsers.findIndex(user => user.id === snap.key);
+      if (index !== -1) {
+        typingUsers = typingUsers.filter(user => user.id !== snap.key);
+        this.setState({ typingUsers });
+      }
+    });
+
+    this.state.connectedRef.on("value", snap => {
+      if (snap.val() === true) {
+        this.state.typingRef
+          .child(channelId)
+          .child(this.state.user.uid)
+          .onDisconnect()
+          .remove(err => {
+            if (err !== null) {
+              console.error(err);
+            }
+          });
+      }
+    });
   };
 
   addMessageListener = channelId => {
@@ -180,6 +219,18 @@ class Messages extends Component {
       : "";
   };
 
+  displayTypingUsers = users =>
+    users.length > 0 &&
+    users.map(user => (
+      <div
+        style={{ display: "flex", alignItems: "center", marginBottom: "0.2em" }}
+        key={user.id}
+      >
+        <span className="user__typing">{user.name} is typing</span>
+        <Typing />
+      </div>
+    ));
+
   render() {
     const {
       messagesRef,
@@ -192,7 +243,8 @@ class Messages extends Component {
       searchResults,
       searchLoading,
       privateChannel,
-      isChannelStarred
+      isChannelStarred,
+      typingUsers
     } = this.state;
 
     return (
@@ -215,10 +267,7 @@ class Messages extends Component {
             {searchTerm
               ? this.displayMessages(searchResults)
               : this.displayMessages(messages)}
-            <div style={{ display: "flex", alignItems: "center" }}>
-              <span className="user__typing">eugenebutenk typing</span>
-              <Typing />
-            </div>
+            {this.displayTypingUsers(typingUsers)}
           </CommentGroup>{" "}
         </Segment>
 
