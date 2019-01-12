933ecedc0212c57ba24956f310d106defb9313e1
JoeVincentt <42827967+JoeVincentt@users.noreply.github.com>
2019-01-12 02:48:47 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import "firebase/storage";
 
 // Initialize Firebase
 const config = {
-  apiKey: "AIzaSyB2Wdt6JyNaeqrQr2l4rqPL60_T9RMQBHw",
-  authDomain: "slack-chat-app-by-eb.firebaseapp.com",
-  databaseURL: "https://slack-chat-app-by-eb.firebaseio.com",
-  projectId: "slack-chat-app-by-eb",
-  storageBucket: "slack-chat-app-by-eb.appspot.com",
-  messagingSenderId: "669048398190"
+  apiKey: "XXX",
+  authDomain: "XXXX",
+  databaseURL: "XXXX",
+  projectId: "XXX",
+  storageBucket: "XXX",
+  messagingSenderId: "XXX"
 };
 firebase.initializeApp(config);
 
